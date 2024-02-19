de4e12a2aa322c8b9f1c74e1725fd4ed584d5054
Priyanshu Mallick <priyanshumallick123456@gmail.com>
2024-02-19 11:53:18 +0530

    Project setup done

---
+++ lib/user_registration.dart
@@ -0,0 +1,279 @@
+import 'package:flutter/material.dart';
+
+class UserRegistration extends StatefulWidget {
+  const UserRegistration({super.key});
+
+  @override
+  State<UserRegistration> createState() => _UserRegistrationState();
+}
+
+class _UserRegistrationState extends State<UserRegistration> {
+  double _animationValue = 1.0; // Variable to control the animation
+
+  // Future<void> initializeFirebase() async {
+  //   await Firebase.initializeApp();
+  //   // _firestore = FirebaseFirestore.instance;
+  // }
+
+  @override
+  void initState() {
+    super.initState();
+    // initializeFirebase();
+    // Start the animation when the widget is created
+    startAnimation();
+  }
+
+  // Function to start the animation
+  void startAnimation() async {
+    // Delay the animation for 500 milliseconds
+    await Future.delayed(Duration(milliseconds: 100));
+    setState(() {
+      _animationValue = 0.0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Stack(
+        children: <Widget>[
+          CustomScrollView(
+            slivers: <Widget>[
+              SliverAppBar(
+                expandedHeight: MediaQuery.of(context).size.height * 0.6,
+                backgroundColor: Colors.black,
+                flexibleSpace: FlexibleSpaceBar(
+                  background: Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('assets/bg.jpg'),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          begin: Alignment.bottomRight,
+                          colors: [
+                            Colors.black,
+                            Colors.black.withOpacity(.2),
+                          ],
+                        ),
+                      ),
+                      child: Padding(
+                        padding: EdgeInsets.only(left: MediaQuery.of(context).size.width * 0.06, bottom: MediaQuery.of(context).size.height * 0.17),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: <Widget>[
+                            TweenAnimationBuilder(
+                              duration: const Duration(milliseconds: 500),
+                              curve: Curves.easeInOut,
+                              tween: Tween<double>(begin: 1, end: _animationValue),
+                              builder: (context, value, child) {
+                                return Transform.translate(
+                                  offset: Offset(
+                                      0, value * MediaQuery.of(context).size.height),
+                                  child: Opacity(
+                                    opacity: 1 - value,
+                                    child: child,
+                                  ),
+                                );
+                              },
+                              child: Text(
+                                "Welcome to",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 40,
+                                ),
+                              ),
+                            ),
+                            TweenAnimationBuilder(
+                              duration: const Duration(milliseconds: 600),
+                              curve: Curves.easeInOut,
+                              tween: Tween<double>(begin: 1, end: _animationValue),
+                              builder: (context, value, child) {
+                                return Transform.translate(
+                                  offset: Offset(
+                                      0, value * MediaQuery.of(context).size.height),
+                                  child: Opacity(
+                                    opacity: 1 - value,
+                                    child: child,
+                                  ),
+                                );
+                              },
+                              child: Text(
+                                "MealsBridge",
+                                style: TextStyle(
+                                  color: Color(0xFF04FC10),
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 40,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: EdgeInsets.only(bottom: MediaQuery.of(context).size.height * 0.2),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TweenAnimationBuilder(
+                    duration: const Duration(milliseconds: 750),
+                    curve: Curves.easeInOut,
+                    tween: Tween<double>(begin: 1, end: _animationValue),
+                    builder: (context, value, child) {
+                      return Transform.translate(
+                        offset: Offset(0, value * MediaQuery.of(context).size.height),
+                        child: Opacity(
+                          opacity: 1 - value,
+                          child: child,
+                        ),
+                      );
+                    },
+                    child: ElevatedButton.icon(
+                      onPressed: () async {
+                        // showDialog(
+                        //   context: context,
+                        //   barrierDismissible: false,
+                        //   builder: (BuildContext context) {
+                        //     return WillPopScope(
+                        //       onWillPop: () async => false, // Disable popping with back button
+                        //       child: const Center(
+                        //         child: SpinKitFadingCircle(
+                        //           color: Color(0xFFFCBB04),
+                        //           size: 50.0,
+                        //         ),
+                        //       ),
+                        //     );
+                        //   },
+                        // );
+                        // await AuthService().signInWithGoogle(context);
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFF04FC10),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20.0),
+                        ),
+                        elevation: 8, // Remove default elevation
+                        shadowColor: const Color(0xFF63FF6A),
+                      ),
+                      icon: Image.asset(
+                        'assets/google.png',
+                        height: MediaQuery.of(context).size.height * 0.03,
+                        width: MediaQuery.of(context).size.height * 0.055,
+                      ),
+                      label: Text('Signin with Google'),
+                    ),
+                  ),
+
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.01),
+
+                  TweenAnimationBuilder(
+                    duration: const Duration(milliseconds: 800),
+                    curve: Curves.easeInOut,
+                    tween: Tween<double>(begin: 1, end: _animationValue),
+                    builder: (context, value, child) {
+                      return Transform.translate(
+                        offset: Offset(0, value * MediaQuery.of(context).size.height),
+                        child: Opacity(
+                          opacity: 1 - value,
+                          child: child,
+                        ),
+                      );
+                    },
+                    child: Center(
+                      child: Text("OR", style: TextStyle(color: Colors.white)),
+                    ),
+                  ),
+
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.01),
+
+                  TweenAnimationBuilder(
+                    duration: const Duration(milliseconds: 850),
+                    curve: Curves.easeInOut,
+                    tween: Tween<double>(begin: 1, end: _animationValue),
+                    builder: (context, value, child) {
+                      return Transform.translate(
+                        offset: Offset(0, value * MediaQuery.of(context).size.height),
+                        child: Opacity(
+                          opacity: 1 - value,
+                          child: child,
+                        ),
+                      );
+                    },
+                    child: ElevatedButton.icon(
+                      onPressed: () {
+                        // Handle Facebook login
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFF04FC10),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20.0),
+                        ),
+                        elevation: 8, // Remove default elevation
+                        shadowColor: const Color(0xFF63FF6A),
+                      ),
+                      icon: Image.asset(
+                        'assets/facebook.png',
+                        height: MediaQuery.of(context).size.height * 0.03,
+                        width: MediaQuery.of(context).size.height * 0.03,
+                      ),
+                      label: Text('Signin with Facebook'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.only(
+                bottom: 20.0,
+                left: 20.0,
+                right: 20.0,
+              ),
+              child: TweenAnimationBuilder(
+                duration: const Duration(milliseconds: 900),
+                curve: Curves.easeInOut,
+                tween: Tween<double>(begin: 1, end: _animationValue),
+                builder: (context, value, child) {
+                  return Transform.translate(
+                    offset: Offset(
+                      0,
+                      value * MediaQuery.of(context).size.height,
+                    ),
+                    child: Opacity(
+                      opacity: 1 - value,
+                      child: child,
+                    ),
+                  );
+                },
+                child: Padding(
+                  padding: EdgeInsets.only(bottom: MediaQuery.of(context).size.height*0.05),
+                  child: Text(
+                    "By signing in, you agree to our Terms and Conditions",
+                    style: TextStyle(color: Colors.white, fontSize: 10, fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
